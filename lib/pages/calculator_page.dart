1b7dc8e30ce941e24c865df3101f7584b0e649c7
Abner Ribeiro da Silva <sabner838@gmail.com>
2021-03-27 22:55:01 -0300

    Feita a logica da calculadora de pisos

---
+++ lib/pages/calculator_page.dart
@@ -0,0 +1,85 @@
+import '../controllers/calculator_controller.dart';
+import 'package:flutter/material.dart';
+import '../core/constants.dart';
+import '../widgets/number_input_field.dart';
+import '../widgets/primary_button.dart';
+import 'result_dialog.dart';
+import '../widgets/text_header.dart';
+
+import '../models/floor_model.dart';
+import '../models/result_model.dart';
+import '../models/room_model.dart';
+
+class CalculatorPage extends StatefulWidget {
+  @override
+  _CalculatorPageState createState() => _CalculatorPageState();
+}
+
+class _CalculatorPageState extends State<CalculatorPage> {
+  final _controller = CalculatorController();
+  final _formKey = GlobalKey<FormState>();
+  final RoomModel _room = RoomModel();
+  final FloorModel _floor = FloorModel();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(kAppTitle),
+      ),
+      body: SingleChildScrollView(
+        child: Form(
+          key: _formKey,
+          child: _buildForm(),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildForm() {
+    return Container(
+      padding: EdgeInsets.all(kSpace),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          TextHeader(label: kEnvironmentHeader, opacity: kOpacityHeader),
+          SizedBox(height: kSpace),
+          NumberInputField(
+            label: '$kWidth ($kMeters)',
+            onSaved: _controller.setRoomWidth,
+          ),
+          SizedBox(height: kSpace),
+          NumberInputField(
+            label: '$kLength ($kMeters)',
+            onSaved: _controller.setRoomLength,
+          ),
+          SizedBox(height: kBigSpace),
+          TextHeader(label: kFloorHeader, opacity: kOpacityHeader),
+          SizedBox(height: kSpace),
+          NumberInputField(
+            label: '$kWidth ($kMeters)',
+            onSaved: _controller.setFloorWidth,
+          ),
+          SizedBox(height: kSpace),
+          NumberInputField(
+            label: '$kLength ($kMeters)',
+            onSaved: _controller.setFloorLength,
+          ),
+          SizedBox(height: kBigSpace),
+          PrimaryButton(
+            label: kCalculateButton,
+            onPressed: _onCalculate,
+          ),
+        ],
+      ),
+    );
+  }
+
+  void _onCalculate() {
+    if (_formKey.currentState.validate()) {
+      _formKey.currentState.save();
+      final result = _controller.calculate();
+      ResultDialog.show(context, result);
+    }
+  }
+}
